b347f118496ef35e7401d049d6b40e4e3bff127a
jaydefense <jay_defense@yahoo.fr>
2017-01-18 22:22:37 +0100

    first commit

---
+++ app/src/main/java/com/application/upnplink/PlayerFragment.java
@@ -0,0 +1,213 @@
+package com.application.upnplink;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PlayerFragment.OnPlayerFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PlayerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PlayerFragment extends Fragment implements View.OnClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private MediaPlayer mediaPlayer;
+    public TextView songName, duration;
+    private double timeElapsed = 0, finalTime = 0;
+    private int forwardTime = 2000, backwardTime = 2000;
+    private Handler durationHandler = new Handler();
+    private SeekBar seekbar;
+    private ImageButton btnPlay, btnPause, btnFf, btnRew;
+
+    private OnPlayerFragmentInteractionListener mListener;
+
+
+    public PlayerFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PlayerFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PlayerFragment newInstance(String param1, String param2) {
+        PlayerFragment fragment = new PlayerFragment();
+        Bundle args = new Bundle();
+
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_player, container, false);
+        songName = (TextView) v.findViewById(R.id.songName);
+        mediaPlayer = MediaPlayer.create(getActivity(), R.raw.alphaville);
+        finalTime = mediaPlayer.getDuration();
+        duration = (TextView) v.findViewById(R.id.songDuration);
+        seekbar = (SeekBar) v.findViewById(R.id.seekBar);
+        songName.setText("alphaville.mp3");
+
+        seekbar.setMax((int) finalTime);
+        seekbar.setClickable(false);
+
+        btnPlay  = (ImageButton)v.findViewById(R.id.media_play);
+        btnPlay.setOnClickListener(this);
+        btnPause = (ImageButton)v.findViewById(R.id.media_pause);
+        btnPause.setOnClickListener(this);
+        btnFf    = (ImageButton)v.findViewById(R.id.media_ff);
+        btnFf.setOnClickListener(this);
+        btnRew   = (ImageButton)v.findViewById(R.id.media_rew);
+        btnRew.setOnClickListener(this);
+
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onPlayerFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnPlayerFragmentInteractionListener) {
+            mListener = (OnPlayerFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    // play mp3 song
+    public void play(View view) {
+        mediaPlayer.start();
+        timeElapsed = mediaPlayer.getCurrentPosition();
+        seekbar.setProgress((int) timeElapsed);
+        durationHandler.postDelayed(updateSeekBarTime, 100);
+    }
+
+    //handler to change seekBarTime
+    private Runnable updateSeekBarTime = new Runnable() {
+        public void run() {
+            //get current position
+            timeElapsed = mediaPlayer.getCurrentPosition();
+            //set seekbar progress
+            seekbar.setProgress((int) timeElapsed);
+            //set time remaing
+            double timeRemaining = finalTime - timeElapsed;
+            duration.setText(String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes((long) timeRemaining), TimeUnit.MILLISECONDS.toSeconds((long) timeRemaining) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) timeRemaining))));
+
+            //repeat yourself that again in 100 miliseconds
+            durationHandler.postDelayed(this, 100);
+        }
+    };
+
+    // pause mp3 song
+    public void pause(View view) {
+        mediaPlayer.pause();
+    }
+
+    // go forward at forwardTime seconds
+    public void rewind(View view) {
+        //check if we can go forward at forwardTime seconds before song endes
+        if ((timeElapsed + forwardTime) < 0) {
+            timeElapsed = timeElapsed - backwardTime;
+
+            //seek to the exact second of the track
+            mediaPlayer.seekTo((int) timeElapsed);
+        }
+    }
+
+    // go forward at forwardTime seconds
+    public void forward(View view) {
+        //check if we can go forward at forwardTime seconds before song endes
+        if ((timeElapsed + forwardTime) <= finalTime) {
+            timeElapsed = timeElapsed + forwardTime;
+
+            //seek to the exact second of the track
+            mediaPlayer.seekTo((int) timeElapsed);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.media_play:
+                play(v);
+                break;
+            case R.id.media_pause:
+                pause(v);
+                break;
+            case R.id.media_ff:
+                forward(v);
+                break;
+            case R.id.media_rew:
+                rewind(v);
+                break;
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnPlayerFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onPlayerFragmentInteraction(Uri uri);
+    }
+}
